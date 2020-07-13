941f953302151e4155c2a894f70be50f93c1b0cc
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-07-12 17:32:12 -0700

    Added notebook file

--- generating_data/PandasTesting.py
+++ generating_data/PandasTesting.py
@@ -2,13 +2,20 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
+# Follow along:
+# https://towardsdatascience.com/basic-time-series-manipulation-with-pandas-4432afee64ea
+
 # A list of panda's Timestamps
 date_range = pd.date_range(start='1/1/2020', end='1/08/2020', freq='H')
 
-print(date_range[0])
-
 df = pd.DataFrame(date_range, columns=['date'])
-
 df['date'] = np.random.randint(0, 100, size=len(date_range))
+print(df.head())
 
-print(df.head(15))
+# Set datetime index
+df['datetime'] = pd.to_datetime(df['date'])
+df = df.set_index('datetime')
+df.drop(['date'], axis=1, inplace=True)
+
+print(df.head())
+
